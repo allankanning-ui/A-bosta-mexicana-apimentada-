5ba563be2f6cbe3dbb54b0aaf5fcbbf301499063
allankanning-ui <allan.kanning@escola.pr.gov.br>
2025-10-29 07:51:42 -0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+
+avanca.forEach(button =>{
+    button.addEventListener('click',function(){
+    const atual =document.querySelector('ativo')
+    const proximoPasso='passo-'+getAtribute('data-proximo')
+    })
+})
